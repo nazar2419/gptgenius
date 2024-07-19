3b354b7d3395a60e144654901442245b79b3c131
Nazar <nazarx095@gmail.com>
2024-07-19 18:40:44 +0300

    sidebar component

--- app/(dashboard)/chat/page.js
+++ app/(dashboard)/chat/page.js
@@ -1,5 +1,5 @@
 const ChatPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline">Chat</h1>;
 };
 
 export default ChatPage;
---
+++ app/components/MemberProfile.jsx
@@ -0,0 +1,15 @@
+import { UserButton } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+const MemberProfile = async () => {
+  const user = await currentUser();
+  // const { userId } = auth();
+  // console.log(userId);
+  return (
+    <div className="px-4 flex items-center gap-2">
+      <UserButton afterSignOutUrl="/" />
+      <p>{user.emailAddresses[0].emailAddresses}</p>
+    </div>
+  );
+};
+export default MemberProfile;
